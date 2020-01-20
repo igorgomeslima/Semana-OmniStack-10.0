4cd7566d37c5e604357f81a777c1fcffeda46e4d
Igor Gomes <igorgomeslima@hotmail.com.br>
2020-01-20 18:19:00 -0300

    17-01(Funcionalidades Avançadas)

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,41 +1,52 @@
-const axios = require('axios');
-const Dev = require('../models/Dev');
-const parseStringAsArray = require('../utils/parseStringAsArray');
+const axios = require("axios");
+const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
 
-    async index(request, response) {
-        const devs = await Dev.find();
+    return response.json(devs);
+  },
 
-        return response.json(devs);
-    },
+  async store(request, response) {
+    const { github_username, techs, latitude, longitude } = request.body;
 
-    async store(request, response) {
+    let dev = await Dev.findOne({ github_username });
 
-        const { github_username, techs, latitude, longitude } = request.body;
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-        let dev = await Dev.findOne({ github_username });
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-        if (!dev) {
-            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+      const techsArray = parseStringAsArray(techs);
 
-            const { name = login, avatar_url, bio } = apiResponse.data;
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
 
-            const location = {
-                type: 'Point',
-                coordinates: [longitude, latitude]
-            };
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
 
-            dev = await Dev.create({
-                github_username,
-                name,
-                avatar_url,
-                bio,
-                techs: parseStringAsArray(techs),
-                location
-            });
-        }
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      );
 
-        return response.json(dev);
+      //console.log(sendSocketMessageTo);
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     }
+
+    return response.json(dev);
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require("http");
+
+const routes = require("./routes");
+const { setupWebsocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
 
-mongoose.connect('mongodb+srv://igor:igor@cluster0-6yb1t.mongodb.net/week10?retryWrites=true&w=majority', {
+setupWebsocket(server);
+
+mongoose.connect(
+  "mongodb+srv://igor:igor@cluster0-6yb1t.mongodb.net/week10?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+  }
+);
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(8888);
+server.listen(8888);
